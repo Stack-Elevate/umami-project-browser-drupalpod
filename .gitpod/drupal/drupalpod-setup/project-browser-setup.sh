cc067ed7f9e5add1a3a7965eab9cd522b314f6af
James Candan <james.candan@stackelevate.dev>
2024-10-28 10:17:33 -0400

    fix: admin toolbar no modules found error

--- .gitpod/drupal/drupalpod-setup/project-browser-setup.sh
+++ .gitpod/drupal/drupalpod-setup/project-browser-setup.sh
@@ -2,7 +2,6 @@
 ddev composer remove --dev drupal/core-dev
 ddev composer remove mglaman/composer-drupal-lenient
 ddev composer require drupal/automatic_updates:^3.1.3
-ddev drush pmu -y admin_toolbar
 ddev drush en -y project_browser package_manager
 ddev drush en -y navigation
 ddev drush cset -y package_manager.settings include_unknown_files_in_project_root true
